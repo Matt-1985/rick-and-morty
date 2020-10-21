ee634913da98aad42255d641a6b58130ea52de94
Matt-1985 <blatsche@web.de>
2020-10-21 13:20:25 +0200

    Set up js body

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import "./app.css";
 import Header from "./components/Header";
 import { createElement } from "./utils/elements";
+import Character from "./components/character";
+import { getCharacterById } from "./utils/api";
 
 function App() {
   const header = Header();
 
-  const main = createElement("main", {
-    innerText: "👋",
+  // const main = createElement("main");
+
+  const character = Character({
+    name: "Rick Sanchez",
+    imgSrc: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
   });
 
-  const container = createElement("div", { children: [header, main] });
+  const character2 = Character({
+    name: "Morty Smith",
+    imgSrc: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+  });
+
+  const main = createElement("main", {
+    children: [character, character2],
+  });
+
+  const container = createElement("div", {
+    children: [header, main],
+  });
+  console.log(getCharacterById(10));
+
   return container;
 }
 
---
+++ src/utils/api.js
@@ -0,0 +1,9 @@
+export async function getCharacterById(id) {
+  const url = `https://rickandmortyapi.com/api/character/${id}`;
+  const response = await fetch(url);
+  const character = await response.json();
+
+  return character;
+}
+
+getCharacterById(10);
